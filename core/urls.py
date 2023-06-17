c9ae883c9a2746d1d3215ebac707955364b93f88
Shishir islam shanto <mdsabirulislam8089@gmail.com>
2023-06-17 22:29:04 +0600

    DB design chagne and patents and add patients page create

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('',home,name="home"),
     path('doctors',doctor,name="doctor-list"),
     path('add-doctor',add_doctor,name="add-doctor"),
+    path('patients',patient,name="patient-list"),
+    path('add-patient',add_patient,name="add-patient"),
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,12 @@ def home(request):
 def doctor(request):
     return render(request,"core/doctor.html")
 
+def patient(request):
+    return render(request,"core/patient.html")
+
 
 def add_doctor(request):
     return render(request,"core/add-doctor.html")
+
+def add_patient(request):
+    return render(request,"core/add-patient.html")
