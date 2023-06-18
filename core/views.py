b73bb10fdd48f646602dc48e79f8ccd33f3362f8
Shishir islam shanto <mdsabirulislam8089@gmail.com>
2023-06-18 23:17:58 +0600

    add patient add

--- core/views.py
+++ core/views.py
@@ -11,7 +11,8 @@ def doctor(request):
     return render(request,"core/doctor.html",locals())
 
 def patient(request):
-    return render(request,"core/patient.html")
+    patients = Patient.objects.order_by("-id")
+    return render(request,"core/patient.html",locals())
 
 
 def add_doctor(request):
@@ -46,6 +47,33 @@ def add_doctor(request):
     return render(request,"core/add-doctor.html",locals())
 
 def add_patient(request):
+    msg = ""
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        dob = request.POST.get("dob")
+        gender = request.POST.get("gender")
+        address = request.POST.get("address")
+        country = request.POST.get("country")
+        city = request.POST.get("city")
+        state = request.POST.get("state")
+        postal_code = request.POST.get("postal_code")
+        phone = request.POST.get("phone")
+        avatar = request.FILES.get("avater")
+        status = request.POST.get("status")
+        User.objects.create_user(username=username,password=password1,first_name=first_name,last_name=last_name,email=email)
+        user = User.objects.order_by("-id")[:1].get()
+        if avatar == None:
+            patient = Patient(user=user,dob=dob,gender=gender,address=address,country=country,city=city,state=state,postal_code=postal_code,phone=phone,status=status)
+            patient.save()
+        else:
+            patient = Patient(user=user,dob=dob,gender=gender,address=address,country=country,city=city,state=state,postal_code=postal_code,phone=phone,avatar=avatar,short_dio=sort_bio,status=status)
+            patient.save()
+        msg = "Patient added successfully."
     return render(request,"core/add-patient.html")
 
 def delete_doctor(request, id):
@@ -56,3 +84,12 @@ def delete_doctor(request, id):
     else:
         doctor.delete()
     return redirect("doctor-list")
+
+def delete_patient(request, id):
+    patient = Patient.objects.get(id=id)
+    if patient.avatar != "default/user.jpg":
+        os.remove(patient.avatar.path)
+        patient.delete()
+    else:
+        patient.delete()
+    return redirect("patient-list")
